23e14814faecc552e7cc0d415b67b0ccb44e1a7a
Alexander! <alexander.osses@gmail.com!>
2023-11-12 15:14:30 -0300

    actualizacion del model

--- models/Post.model.js
+++ models/Post.model.js
@@ -1,33 +1,43 @@
 const mongoose = require('mongoose')
 
 const postSchema = new mongoose.Schema({
-    URL: { type: String },
-    DESCRIPCION: { type: String },
-    PRECIO: { type: Number },
-    PRECIO_OFERTA: { type: Number },
-    DESCUENTO: { type: Number },
-    PESO: { type: Number },
-    MARCA: { type: String },
-    MODELO: { type: String },
-    IMG: { type: String },
-    BATERIA: { type: String },
-    MOTOR: { type: String },
-    CARGA_MAX: { type: Number },
-    TIEMPO_DE_CARGA: { type: Number },
-    VELOCIDAD_MAX: { type: Number },
-    AUTONOMIA: { type: Number },
-    LUCES: { type: String },
-    SUSPENSION: { type: String },
-    FRENOS: { type: String },
-    NEUMATICO: { type: String },
-    MEDIDAS_DESPLEGADO: { type: String },
-    MEDIDA_PLEGADO: { type: String },
-    PENDIENTE: { type: Number },
-    MATERIAL_DEL_PRODUCTO: { type: String },
-    DISPLAY: { type: String },
-    category: { type: mongoose.ObjectId, ref: 'Category' },
-    user: { type: mongoose.ObjectId, ref: 'User' }
-})
+
+    // Información del producto
+    descripcion: { type: String, required: true }, // Descripción del producto
+    precio: { type: Number, required: true }, // Precio del producto
+    precioOferta: { type: Number }, // Precio en oferta (opcional)
+    descuento: { type: Number, min: 0, max: 100 }, // Descuento aplicado (en porcentaje)
+
+    // Especificaciones técnicas
+    peso: { type: Number }, // Peso del producto
+    marca: { type: String }, // Marca del producto
+    modelo: { type: String }, // Modelo del producto
+    img: { type: String }, // URL de la imagen del producto
+
+    // Detalles adicionales
+    bateria: { type: String }, // Tipo de batería
+    motor: { type: String }, // Tipo de motor
+    cargaMax: { type: Number }, // Carga máxima que soporta el producto
+    tiempoDeCarga: { type: Number }, // Tiempo de carga de la batería
+    velocidadMax: { type: Number }, // Velocidad máxima del producto
+    autonomia: { type: Number }, // Autonomía de la batería
+
+    // Características adicionales
+    luces: { type: String }, // Tipo de luces
+    suspension: { type: String }, // Tipo de suspensión
+    frenos: { type: String }, // Tipo de frenos
+    neumatico: { type: String }, // Tipo de neumático
+
+    // Dimensiones y plegado
+    medidasDesplegado: { type: String }, // Medidas cuando el producto está desplegado
+    medidaPlegado: { type: String }, // Medidas cuando el producto está plegado
+
+    // Otros detalles
+    pendiente: { type: Number }, // Pendiente máxima que puede subir el producto
+    materialDelProducto: { type: String }, // Material del que está hecho el producto
+    display: { type: String }, // Tipo de pantalla o display (si aplica)
+},
+{ timestamps: true });
 
 const Post = mongoose.model('Post', postSchema)
 
