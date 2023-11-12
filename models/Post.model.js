26337d5d25ce4989bb648740a6ae9450dce4e022
Alexander! <alexander.osses@gmail.com!>
2023-11-12 16:24:37 -0300

    nueva actualizacion del archivo

--- models/Post.model.js
+++ models/Post.model.js
@@ -1,41 +1,29 @@
 const mongoose = require('mongoose')
 
 const postSchema = new mongoose.Schema({
-
-    // Información del producto
-    descripcion: { type: String, required: true }, // Descripción del producto
-    precio: { type: Number, required: true }, // Precio del producto
-    precioOferta: { type: Number }, // Precio en oferta (opcional)
-    descuento: { type: Number, min: 0, max: 100 }, // Descuento aplicado (en porcentaje)
-
-    // Especificaciones técnicas
-    peso: { type: Number }, // Peso del producto
-    marca: { type: String }, // Marca del producto
-    modelo: { type: String }, // Modelo del producto
-    img: { type: String }, // URL de la imagen del producto
-
-    // Detalles adicionales
-    bateria: { type: String }, // Tipo de batería
-    motor: { type: String }, // Tipo de motor
-    cargaMax: { type: Number }, // Carga máxima que soporta el producto
-    tiempoDeCarga: { type: Number }, // Tiempo de carga de la batería
-    velocidadMax: { type: Number }, // Velocidad máxima del producto
-    autonomia: { type: Number }, // Autonomía de la batería
-
-    // Características adicionales
-    luces: { type: String }, // Tipo de luces
-    suspension: { type: String }, // Tipo de suspensión
-    frenos: { type: String }, // Tipo de frenos
-    neumatico: { type: String }, // Tipo de neumático
-
-    // Dimensiones y plegado
-    medidasDesplegado: { type: String }, // Medidas cuando el producto está desplegado
-    medidaPlegado: { type: String }, // Medidas cuando el producto está plegado
-
-    // Otros detalles
-    pendiente: { type: Number }, // Pendiente máxima que puede subir el producto
-    materialDelProducto: { type: String }, // Material del que está hecho el producto
-    display: { type: String }, // Tipo de pantalla o display (si aplica)
+    descripcion: { type: String },
+    precio: { type: Number }, 
+    precioOferta: { type: Number }, 
+    descuento: { type: Number, min: 0, max: 100 }, 
+    peso: { type: Number },
+    marca: { type: String },
+    modelo: { type: String },
+    img: { type: String },
+    bateria: { type: String }, 
+    motor: { type: String }, 
+    cargaMax: { type: Number }, 
+    tiempoDeCarga: { type: Number }, 
+    velocidadMax: { type: Number },
+    autonomia: { type: Number }, 
+    luces: { type: String },
+    suspension: { type: String },
+    frenos: { type: String },
+    neumatico: { type: String },
+    medidasDesplegado: { type: String },
+    medidaPlegado: { type: String },
+    pendiente: { type: Number },
+    materialDelProducto: { type: String },
+    display: { type: String },
 },
 { timestamps: true });
 
